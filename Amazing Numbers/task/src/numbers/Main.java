fac01cd56422b4f7633264a1664bb88d4182e325
KronosGR <geo.elgeo@gmail.com>
2022-11-03 12:48:12 +0100

    stage 2/8

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -2,7 +2,7 @@ package numbers;
 
 import java.util.Scanner;
 
-class Buzz {
+class AmazingNumbers {
     private Scanner sc = new Scanner(System.in);
     private int num;
 
@@ -18,40 +18,59 @@ class Buzz {
         }
     }
 
-    public void isEven() {
-        if (num % 2 == 0) {
-            System.out.println("The number is Even.");
-        } else {
-            System.out.println("The number is Odd.");
-        }
+    private boolean isEven() {
+//        if (num % 2 == 0) {
+//            System.out.println("The number is Even.");
+//        } else {
+//            System.out.println("The number is Odd.");
+//        }
+        return num % 2 == 0;
     }
 
-    public void isBuzz() {
-        String msg = "";
-        String explanation = "";
+    private boolean isOdd() {
+        return num % 2 != 0;
+    }
+
+    private boolean isBuzz() {
+//        String msg = "";
+//        String explanation = "";
         if (num % 7 != 0 && String.valueOf(num).charAt(String.valueOf(num).length() - 1) != 55) {
-            explanation = num + " is neither divisible by 7 nor does it end with 7.";
-            msg = "It is not a Buzz number.";
+//            explanation = num + " is neither divisible by 7 nor does it end with 7.";
+//            msg = "It is not a Buzz number.";
+            return false;
         } else if (num % 7 == 0 && String.valueOf(num).charAt(String.valueOf(num).length() - 1) == 55) {
-            explanation = num + " is divisible by 7 and ends with 7.";
-            msg = "It is a Buzz number.";
+//            explanation = num + " is divisible by 7 and ends with 7.";
+//            msg = "It is a Buzz number.";
+            return true;
         } else if (num % 7 == 0) {
-            explanation = num + " is divisible by 7.";
-            msg = "It is a Buzz number.";
+//            explanation = num + " is divisible by 7.";
+//            msg = "It is a Buzz number.";
+            return true;
         } else if (String.valueOf(num).charAt(String.valueOf(num).length() - 1) == 55) {
-            explanation = num + " ends with 7";
-            msg = "It is a Buzz number.";
+//            explanation = num + " ends with 7";
+//            msg = "It is a Buzz number.";
+            return true;
         }
-        System.out.println(msg);
-        System.out.println("Explanation:");
-        System.out.println(explanation);
+//        System.out.println(msg);
+//        System.out.println("Explanation:");
+//        System.out.println(explanation);
+        return false;
+    }
+
+    private boolean isDuck(){
+        String tmp = String.valueOf(num).substring(1);
+        return tmp.contains("0");
     }
 
     public void start() {
         askNumber();
         isNatural();
-        isEven();
-        isBuzz();
+        System.out.println("Properties of "+ num);
+        System.out.println("        even: "+ isEven());
+        System.out.println("         odd: "+ isOdd());
+        System.out.println("        buzz: "+ isBuzz());
+        System.out.println("        duck: "+ isDuck());
+
     }
 }
 
@@ -59,7 +78,7 @@ public class Main {
     public static void main(String[] args) {
 //        write your code here
 
-        Buzz buzz = new Buzz();
-        buzz.start();
+        AmazingNumbers aNum = new AmazingNumbers();
+        aNum.start();
     }
 }
